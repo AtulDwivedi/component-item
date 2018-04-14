22f30f509db03db2f55b4bd03e5f4d580116f24f
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-04-14 08:38:28 +0530

    More item operation

--- src/main/java/com/atuldwivedi/quickcart/component/item/ItemFeignClient.java
+++ src/main/java/com/atuldwivedi/quickcart/component/item/ItemFeignClient.java
@@ -1,9 +1,8 @@
 package com.atuldwivedi.quickcart.component.item;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,5 +13,11 @@ public interface ItemFeignClient {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public Resources getItems();
+	
+	@RequestMapping(value = "/{itemId}", method = RequestMethod.GET)
+	public Resources getItems(@PathVariable("itemId") Long itemId);
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public Resources createItem(Item item);
 
 }
--- src/main/java/com/atuldwivedi/quickcart/component/item/controller/ItemController.java
+++ src/main/java/com/atuldwivedi/quickcart/component/item/controller/ItemController.java
@@ -1,18 +1,20 @@
 package com.atuldwivedi.quickcart.component.item.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.atuldwivedi.quickcart.component.item.ItemFeignClient;
@@ -26,17 +28,29 @@ public class ItemController {
 	private ItemFeignClient itemFeignClient;
 
 	@GetMapping(value = "/")
-	public String showHomePage(Map<String, List<Item>> map) {
+	public String getItems(Map<String, List<Item>> map) {
 		Resources itemsRsrc = itemFeignClient.getItems();
 		itemsRsrc.removeLinks();
 		List<Item> items = new ArrayList<>(itemsRsrc.getContent());
-//		Collection<> itemsC = itemsRsrc.getContent();
-//		itemsC.
-//		List<Item> items = null;
-//		itemsC.forEach(it -> items = new ArrayList<Item>(it));
 
 		map.put("items", items);
 		return "items";
+	}
+
+	@GetMapping(value = "/add")
+	public String showAddItemForm(Model model) {
+		Item item = new Item();
+		model.addAttribute("item", item);
+		return "add-item";
+	}
+
+	@PostMapping(value = "/add")
+	public String processAddItem(@Valid @ModelAttribute("item") Item item, BindingResult bindingResult) {
+		System.out.println(bindingResult.getAllErrors());
+		System.out.println(itemFeignClient.createItem(item).getContent());
+		List<Item> itm = new ArrayList<>(itemFeignClient.getItems(1L).getContent());
+		System.out.println(itm);
+		return "index";
 	}
 
 }
